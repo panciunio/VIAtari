b84edb7bcebb57a2a6873621533a0e585b8824cb
panciunio <pancio@pancio.net>
2019-08-22 19:28:29 +0200

    20190822 v1.0.10

--- S1D15705.asm
+++ S1D15705.asm
@@ -23,9 +23,7 @@
 ;
 
 
-;Load S1D15705 registers description
- 
-            ICL "S1D15705.inc"
+
  
  
 ;VIA initialisation:
@@ -52,7 +50,9 @@ lcd_write_cmd:
             and #S1D15705_WR_N
             sta S1D15705_CTRLPORT               ; send command
             
-            nop
+            ;nop
+            ;nop
+            ;nop
             nop
             
             ora #S1D15705_WR
@@ -77,9 +77,9 @@ lcd_write_data:
             and #S1D15705_WR_N
             sta S1D15705_CTRLPORT               ; send data
             
-            nop
-            nop
-            nop
+            ;nop
+            ;nop
+            ;nop
             nop
 
             ora #S1D15705_WR
@@ -118,35 +118,9 @@ lcd_read_stat:
             
 
 ;lcd_clear:
-
-lcd_clear:  ldy #(S1D_SETPAGE)
-clearloop1  tya
-            jsr lcd_write_cmd
-            lda #S1D_STARTLINE
-            jsr lcd_write_cmd        
-            
-            jsr cfill
-            
-            iny
-            cpy #(S1D_SETPAGE +9)
-            bne clearloop1
-            rts
-            
-cfill       lda #S1D_SETCOLL_L
-            jsr lcd_write_cmd
-        
-            ldx #$00
-            lda #$00
-
-floop       jsr lcd_write_data
-            inx
-            cpx #170
-            bne floop
-            rts
-        
-        
 ;lcd_pfill:
-        
+lcd_clear:  lda #$00
+            sta pattern
 lcd_pfill:  ldy #(S1D_SETPAGE)
 fillploop1  tya
             jsr lcd_write_cmd
@@ -172,21 +146,184 @@ pfloop  jsr     lcd_write_data
         bne pfloop
         rts
 
+;frm2lcd - send frame from RAM to LCD memory
+;
+frm2lcd:
 
+        pha
+        txa
+        pha
+        tya
+        pha
+        
+        lda     #(S1D_SETPAGE0)
+        jsr     lcd_write_cmd       
+        lda     #S1D_STARTLINE
+        jsr     lcd_write_cmd       ; set first column
+        lda     #(S1D_SETCOLL_L+3)
+        jsr     lcd_write_cmd
+        ldx     #$00
+page0   lda     lcd_page0,x
+        jsr     lcd_write_data
+        inx
+        cpx     #(LCD_MAX_COLlS)   
+        bne     page0
+        
+        lda     #(S1D_SETPAGE1)
+        jsr     lcd_write_cmd       
+        lda     #S1D_STARTLINE
+        jsr     lcd_write_cmd       ; set first column
+        lda     #(S1D_SETCOLL_L+3)
+        jsr     lcd_write_cmd
+        ldx     #$00
+page1   lda     lcd_page1,x
+        jsr     lcd_write_data
+        inx
+        cpx     #(LCD_MAX_COLlS)   
+        bne     page1     
+        
+        lda     #(S1D_SETPAGE2)
+        jsr     lcd_write_cmd       
+        lda     #S1D_STARTLINE
+        jsr     lcd_write_cmd       ; set first column
+        lda     #(S1D_SETCOLL_L+3)
+        jsr     lcd_write_cmd
+        ldx     #$00
+page2   lda     lcd_page2,x
+        jsr     lcd_write_data
+        inx
+        cpx     #(LCD_MAX_COLlS)
+        bne     page2
+        
+        lda     #(S1D_SETPAGE3)
+        jsr     lcd_write_cmd       
+        lda     #S1D_STARTLINE
+        jsr     lcd_write_cmd       ; set first column
+        lda     #(S1D_SETCOLL_L+3)
+        jsr     lcd_write_cmd
+        ldx     #$00
+page3   lda     lcd_page3,x
+        jsr     lcd_write_data
+        inx
+        cpx     #(LCD_MAX_COLlS)
+        bne     page3
+        
+        lda     #(S1D_SETPAGE4)
+        jsr     lcd_write_cmd       
+        lda     #S1D_STARTLINE
+        jsr     lcd_write_cmd       ; set first column
+        lda     #(S1D_SETCOLL_L+3)
+        jsr     lcd_write_cmd
+        ldx     #$00
+page4   lda     lcd_page4,x
+        jsr     lcd_write_data
+        inx
+        cpx     #(LCD_MAX_COLlS)
+        bne     page4
+        
+        lda     #(S1D_SETPAGE5)
+        jsr     lcd_write_cmd       
+        lda     #S1D_STARTLINE
+        jsr     lcd_write_cmd       ; set first column
+        lda     #(S1D_SETCOLL_L+3)
+        jsr     lcd_write_cmd
+        ldx     #$00
+page5   lda     lcd_page5,x
+        jsr     lcd_write_data
+        inx
+        cpx     #(LCD_MAX_COLlS)
+        bne     page5
+        
+        lda     #(S1D_SETPAGE6)
+        jsr     lcd_write_cmd       
+        lda     #S1D_STARTLINE
+        jsr     lcd_write_cmd       ; set first column
+        lda     #(S1D_SETCOLL_L+3)
+        jsr     lcd_write_cmd
+        ldx     #$00
+page6   lda     lcd_page6,x
+        jsr     lcd_write_data
+        inx
+        cpx     #(LCD_MAX_COLlS)
+        bne     page6
+        
+        lda     #(S1D_SETPAGE7)
+        jsr     lcd_write_cmd       
+        lda     #S1D_STARTLINE
+        jsr     lcd_write_cmd       ; set first column
+        lda     #(S1D_SETCOLL_L+3)
+        jsr     lcd_write_cmd
+        ldx     #$00
+page7   lda     lcd_page7,x
+        jsr     lcd_write_data
+        inx
+        cpx     #(LCD_MAX_COLlS)
+        bne     page7 
+        
+        pla
+        tay
+        pla
+        tax
+        pla               
+        rts
+
+frmclear:
+        ldx #$0
+        lda #$0
+        sta pattern
+frmfill:
+        lda pattern
+frmfl   sta lcd_page0,x
+        sta lcd_page1,x
+        sta lcd_page2,x
+        sta lcd_page3,x
+        sta lcd_page4,x
+        sta lcd_page5,x
+        sta lcd_page6,x
+        sta lcd_page7,x
+        inx
+        cpx #(LCD_MAX_COLLS)
+        bne frmfl
+        rts
+        
+;C-color (0..1), Y-x coord (0..161), X-y coord (0..63)
+
+plot:
+        lda rowadl,x
+        sta adr
+        lda rowadh,x
+        sta adr+1
+
+        lda (adr),y
+        and masktab,x
+        scc
+        ora pixeltab,x
+        sta (adr),y
+        rts
+
+        
 ;lcd_init:
 
-lcd_init:   lda #$ab        ; oscillator ON
+lcd_init:   
+            lda #$00
+            sta S1D15705_CTRLPORT   ;perform RESET
+            
+            nop
+            nop
+            nop            
+                        
+            lda #$ab        ; oscillator ON
             jsr lcd_write_cmd 
                 
             lda #$a3        ; LCD bias value   1/9
             jsr lcd_write_cmd
             
-            lda #$a7        ; enable inversion
+            lda #S1D_DISROT_RR      ; enable inversion
             jsr lcd_write_cmd
             
-            lda #$c8        ; disable mirroring
+            lda #S1D_COMOUTSS_RR    ; disable mirroring
             jsr lcd_write_cmd
-          
+
             lda #$40        ; line 0
             jsr lcd_write_cmd
    
@@ -210,5 +347,9 @@ lcd_init:   lda #$ab        ; oscillator ON
             
             lda #$a4        ; All lighting OFF
             jsr lcd_write_cmd
-            
+
             rts
+            
+;Load S1D15705 registers description
+ 
+            ICL "S1D15705.inc"
--- main.asm
+++ main.asm
@@ -36,10 +36,6 @@ random  equ $d20a
 ;
 ;lcd_write_data
 ;   A - data to send to display memory
-;
-
-
-
 
 ; waitkbs:  
 ;   wait for key
@@ -47,9 +43,16 @@ random  equ $d20a
 ;
 
 
+;variables on 0 page        
+        org $80
+        
+adr     .ds 2
+x       .ds 1
+y       .ds 1
+a       .ds 1       
 
 
-         ORG   $6000
+        org   $6000
          
 ;main program         
         
@@ -61,8 +64,9 @@ start   jsr     via_init            ; set VIA ports
         lda     #$ff               ; write pattern to LCD memory
         sta     pattern
         jsr     lcd_pfill
+        
         lda     #$21
-        jsr     waitkbs
+        ;jsr     waitkbs
 
  ;test lcd 
                 
@@ -77,11 +81,11 @@ charl0  lda     font5x8,x
         jsr     frm2lcd
          
         lda     #$21
-        jsr     waitkbs     
+        ;jsr     waitkbs     
  
         
         ldx     #$00
-charl1  lda     $e200,x
+charl1  lda     $e000,x
         sta     lcd_page1,x
         inx
         cpx     #(LCD_MAX_COLLS - 2)      
@@ -89,10 +93,8 @@ charl1  lda     $e200,x
         
         jsr     frm2lcd
         
-        
-        
         lda     #$21
-        jsr     waitkbs
+        ;jsr     waitkbs
 
         
         ldx     #$00                ; get data from Atari chars generator (ROM)
@@ -105,12 +107,12 @@ charl2  lda     $e100,x
         jsr     frm2lcd
         
         lda     #$21
-        jsr     waitkbs
+        ;jsr     waitkbs
 
 
         
         ldx     #$00
-charl3  lda     $e100,x
+charl3  lda     $e200,x
         sta     lcd_page3,x
         inx
         cpx     #(LCD_MAX_COLLS - 2)    
@@ -119,12 +121,12 @@ charl3  lda     $e100,x
         jsr     frm2lcd
        
         lda     #$21
-        jsr     waitkbs             
+        ;jsr     waitkbs             
 
 
         
         ldx     #$00
-charl4  lda     $e100,x
+charl4  lda     $e300,x
         sta     lcd_page4,x
         inx
         cpx     #(LCD_MAX_COLLS - 2)     
@@ -133,7 +135,7 @@ charl4  lda     $e100,x
         jsr     frm2lcd
 
         lda     #$21
-        jsr     waitkbs     
+        ;jsr     waitkbs     
         
 
         
@@ -148,140 +150,182 @@ charl5  lda     font8x8,x
         jsr     frm2lcd
         
         lda     #$21
-        jsr     waitkbs 
+        ;jsr     waitkbs 
 
 
 
         
-mloop   ;jsr     frm2lcd     ;show frame
-
-        lda     random
-        sta     pattern
-        jsr     lcd_pfill
+mloop:
+        ldx     #$00
+ml0     lda     random
+        adc     #$aa
+        sta     lcd_page7,x
+        and     random
+        sta     lcd_page6,x
+        and     random
+        sta     lcd_page5,x
+        and     random
+        sta     lcd_page4,x
+        and     random
+        sta     lcd_page3,x
+        and     random
+        sta     lcd_page2,x
+        and     random
+        sta     lcd_page1,x
+        and     random
+        sta     lcd_page0,x
+        inx
+        cpx     #LCD_MAX_COLLS +1 
+        bne     ml0
+        
+        jsr     frm2lcd
         
         lda     keycode
-        cmp     #$0c
-        beq     stop
+        cmp     #$21
+        beq     next
             
         jmp     mloop
+        
+next:
+        jsr frmclear
+        
+ 
+        lda #$00
+        tax
+        tay
+        
+ploop   sty x
+        stx y
+        sta a
+        
+        sec
+        jsr plot
 
-stop:
+        
+        lda a
+        ldx y
+        ldy x
+        
+        iny
+        cpy #$a2
+        bne ploop
+
+        jsr frm2lcd
+
+        ldy #$00
+        
+        inx
+        cpx #$40
+        bne ploop
+
+        ldx #$00
+        ldy #$00
+
+ploop2  txs
+        tya
+        pha
+        
+        clc
+        jsr plot
+
+        
+        pla
+        tay
+        tsx
+        
+        iny
+        cpy #$a2
+        bne ploop2
+
+        jsr frm2lcd
+
+        ldy #$00
+        
+        inx
+        cpx #$40
+        bne ploop2 
+        
+
+
+_plop3  lda #$00
+        tax
+        tay
+
+ploop3  sty x
+        stx y
+        sta a
+        
+        lda random
+        rol
+        ;sec
+        jsr plot
+
+        
+        lda a
+        ldx y
+        ldy x
+        
+        iny
+        cpy #$a2
+        bne ploop3
+
+        ;jsr frm2lcd
+
+        ldy #$00
+        
+        inx
+        cpx #$40
+        bne ploop3
+        
+        jsr frm2lcd
+        
+
+        ldx #$00
+        ldy #$00
+
+ploop4  txs
+        tya
+        pha
+
+        lda random
+        rol        
+        ;clc
+        jsr plot
+
+        
+        pla
+        tay
+        tsx
+        
+        iny
+        cpy #$a2
+        bne ploop4
+
+        ;jsr frm2lcd
+
+        ldy #$00
+        
+        inx
+        cpx #$40
+        bne ploop4 
+        
+        jsr frm2lcd
+        
+        lda  keycode
+        cmp #$0c
+        bne _plop3
+         
+               
+        
+         
+stop:   
+        jsr frmclear
+        jsr frm2lcd
         brk
 
 
 
 ;subroutines
-
-
-frm2lcd:
-        lda     #(S1D_SETPAGE0)
-        jsr     lcd_write_cmd       
-        lda     #S1D_STARTLINE
-        jsr     lcd_write_cmd       ; set first column
-        lda     #(S1D_SETCOLL_L+3)
-        jsr     lcd_write_cmd
-        ldx     #$00
-page0   lda     lcd_page0,x
-        jsr     lcd_write_data
-        inx
-        cpx     #(LCD_MAX_COLlS - 2)    ;don't fill last 2 colls  
-        bne     page0
-        
-        lda     #(S1D_SETPAGE1)
-        jsr     lcd_write_cmd       
-        lda     #S1D_STARTLINE
-        jsr     lcd_write_cmd       ; set first column
-        lda     #(S1D_SETCOLL_L+3)
-        jsr     lcd_write_cmd
-        ldx     #$00
-page1   lda     lcd_page1,x
-        jsr     lcd_write_data
-        inx
-        cpx     #(LCD_MAX_COLlS - 2)    ;don't fill last 2 colls  
-        bne     page1     
-        
-        lda     #(S1D_SETPAGE2)
-        jsr     lcd_write_cmd       
-        lda     #S1D_STARTLINE
-        jsr     lcd_write_cmd       ; set first column
-        lda     #(S1D_SETCOLL_L+3)
-        jsr     lcd_write_cmd
-        ldx     #$00
-page2   lda     lcd_page3,x
-        jsr     lcd_write_data
-        inx
-        cpx     #(LCD_MAX_COLlS - 2)    ;don't fill last 2 colls  
-        bne     page2
-        
-        lda     #(S1D_SETPAGE3)
-        jsr     lcd_write_cmd       
-        lda     #S1D_STARTLINE
-        jsr     lcd_write_cmd       ; set first column
-        lda     #(S1D_SETCOLL_L+3)
-        jsr     lcd_write_cmd
-        ldx     #$00
-page3   lda     lcd_page3,x
-        jsr     lcd_write_data
-        inx
-        cpx     #(LCD_MAX_COLlS - 2)    ;don't fill last 2 colls  
-        bne     page3
-        
-        lda     #(S1D_SETPAGE4)
-        jsr     lcd_write_cmd       
-        lda     #S1D_STARTLINE
-        jsr     lcd_write_cmd       ; set first column
-        lda     #(S1D_SETCOLL_L+3)
-        jsr     lcd_write_cmd
-        ldx     #$00
-page4   lda     lcd_page4,x
-        jsr     lcd_write_data
-        inx
-        cpx     #(LCD_MAX_COLlS - 2)    ;don't fill last 2 colls  
-        bne     page4
-        
-        lda     #(S1D_SETPAGE5)
-        jsr     lcd_write_cmd       
-        lda     #S1D_STARTLINE
-        jsr     lcd_write_cmd       ; set first column
-        lda     #(S1D_SETCOLL_L+3)
-        jsr     lcd_write_cmd
-        ldx     #$00
-page5   lda     lcd_page5,x
-        jsr     lcd_write_data
-        inx
-        cpx     #(LCD_MAX_COLlS - 2)    ;don't fill last 2 colls  
-        bne     page5
-        
-        lda     #(S1D_SETPAGE6)
-        jsr     lcd_write_cmd       
-        lda     #S1D_STARTLINE
-        jsr     lcd_write_cmd       ; set first column
-        lda     #(S1D_SETCOLL_L+3)
-        jsr     lcd_write_cmd
-        ldx     #$00
-page6   lda     lcd_page6,x
-        jsr     lcd_write_data
-        inx
-        cpx     #(LCD_MAX_COLlS - 2)    ;don't fill last 2 colls  
-        bne     page6
-        
-        lda     #(S1D_SETPAGE7)
-        jsr     lcd_write_cmd       
-        lda     #S1D_STARTLINE
-        jsr     lcd_write_cmd       ; set first column
-        lda     #(S1D_SETCOLL_L+3)
-        jsr     lcd_write_cmd
-        ldx     #$00
-page7   lda     lcd_page7,x
-        jsr     lcd_write_data
-        inx
-        cpx     #(LCD_MAX_COLlS - 2)    ;don't fill last 2 colls  
-        bne     page7
-        
-        rts
-        
-
-
 waitkbs:
         pha
               
@@ -293,9 +337,10 @@ waitkbs:
 kbloop  cmp     keycode
         bne     kbloop
         
-        rts
+        lda     #$ff
+        sta     keycode
         
-
+        rts
 
         ICL "VIA6522.asm"
         ICL "S1D15705.asm"
